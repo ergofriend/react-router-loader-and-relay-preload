51576b96766436da1340ab94afe9c84ba329c40a
ErgoFriend <erg.friend@gmail.com>
2022-06-04 22:02:58 +0900

    replace loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,22 @@
+import { Suspense } from "react";
+import { useRelayEnvironment } from "react-relay";
 import { DataBrowserRouter, Route } from "react-router-dom";
 import "./App.css";
-import { MissionList } from "./pages/MissionList";
+import { MissionList, missionListQueryLoader } from "./pages/MissionList";
 
 function App() {
+  const environment = useRelayEnvironment();
   return (
     <div className="App">
-      <DataBrowserRouter fallbackElement={<></>}>
-        <Route path="/" element={<MissionList />} />
-      </DataBrowserRouter>
+      <Suspense fallback={<div>Suspense</div>}>
+        <DataBrowserRouter fallbackElement={<></>}>
+          <Route
+            path="/"
+            element={<MissionList />}
+            loader={() => missionListQueryLoader(environment)}
+          />
+        </DataBrowserRouter>
+      </Suspense>
     </div>
   );
 }
--- src/pages/MissionList.tsx
+++ src/pages/MissionList.tsx
@@ -1,5 +1,7 @@
 import graphql from "babel-plugin-relay/macro";
-import { useLazyLoadQuery } from "react-relay";
+import { loadQuery, PreloadedQuery, usePreloadedQuery } from "react-relay";
+import { useLoaderData } from "react-router-dom";
+import { MissionListQuery } from "./__generated__/MissionListQuery.graphql";
 
 const missionListQuery = graphql`
   query MissionListQuery {
@@ -10,8 +12,21 @@ const missionListQuery = graphql`
   }
 `;
 
+export const missionListQueryLoader = (environment: any) =>
+  loadQuery(environment, missionListQuery, {});
+
 export const MissionList = () => {
-  const data = useLazyLoadQuery(missionListQuery, {});
+  const _data = useLoaderData() as PreloadedQuery<MissionListQuery>;
+  const data = usePreloadedQuery<MissionListQuery>(missionListQuery, _data);
   console.log(data);
-  return <div>MissionList</div>;
+  return (
+    <div>
+      MissionList
+      <div>
+        {data.missions?.map(
+          (mission) => mission && <div key={mission.id}>{mission.name}</div>
+        )}
+      </div>
+    </div>
+  );
 };
