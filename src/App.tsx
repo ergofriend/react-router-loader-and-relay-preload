5a7ac807d90d6f0513706208e631fec1b3966244
ErgoFriend <erg.friend@gmail.com>
2022-06-06 13:09:51 +0900

    cache useLoaderData

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import { AnimatePresence } from "framer-motion";
-import { useContext, useState } from "react";
-import {
-  useLocation,
-  useOutlet,
-  UNSAFE_DataRouterContext,
-} from "react-router-dom";
+import { Suspense, useState } from "react";
+import { useLocation, useOutlet } from "react-router-dom";
 import "./App.css";
 import { PageWrapper } from "./components/PageWrapper";
 
-const AnimatedOutlet: React.FC = () => {
-  let [loaderContext] = useState(useContext(UNSAFE_DataRouterContext));
-  return (
-    <UNSAFE_DataRouterContext.Provider value={loaderContext}>
-      <AnimatedOutletInner />
-    </UNSAFE_DataRouterContext.Provider>
-  );
-};
+type AnimatedOutletProps = {};
 
-const AnimatedOutletInner = () => {
+const AnimatedOutlet: React.FC<AnimatedOutletProps> = () => {
   const o = useOutlet();
   const [outlet] = useState(o);
-  return outlet;
+  return <Suspense fallback={<div>Suspense</div>}>{outlet}</Suspense>;
 };
 
 function App() {
--- src/hooks/useInAppPreloadedQuery.ts
+++ src/hooks/useInAppPreloadedQuery.ts
@@ -1,12 +1,28 @@
 import { OperationType } from "relay-runtime";
 import { GraphQLTaggedNode, PreloadedQuery, usePreloadedQuery } from "react-relay";
 import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const useInAppLoaderData = <TQuery extends OperationType>() => {
+  try {
+    const _data = useLoaderData() as PreloadedQuery<TQuery>;
+    return _data
+  } catch (error) {
+    console.log(error)
+  }
+  return null
+}
 
 export const useInAppPreloadedQuery = <TQuery extends OperationType>(
   query: GraphQLTaggedNode,
   preloadedQuery?: PreloadedQuery<TQuery>
 ) => {
-  const _data = useLoaderData() as PreloadedQuery<TQuery>;
-  const data = usePreloadedQuery<TQuery>(query, _data ?? preloadedQuery);
+  const _data = useInAppLoaderData<TQuery>()
+  const [_cache, setCache] = useState<PreloadedQuery<TQuery>>(_data!)
+  useEffect(() => {
+    _data && setCache(_data)
+  }, [_data])
+
+  const data = usePreloadedQuery<TQuery>(query, _cache ?? preloadedQuery);
   return data;
 };
