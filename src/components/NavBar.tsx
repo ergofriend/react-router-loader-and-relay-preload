100fe690f02cd9632f7e77689ed3d66f5fe29eec
ErgoFriend <erg.friend@gmail.com>
2022-06-19 15:34:09 +0900

    fix NavBar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,16 +6,27 @@ export const NavBar = () => {
   const navigate = useNavigate();
 
   return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <p>NavBar</p>
-      <p onClick={() => navigate(-1)}>navigate(-1)</p>
-      <p onClick={() => navigate(-2)}>navigate(-2)</p>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        backgroundColor: "salmon",
+        margin: "1rem",
+      }}
+    >
+      <div>NavBar</div>
+      <button onClick={() => navigate(-1)} style={{ width: "100px" }}>
+        navigate(-1)
+      </button>
+      <button onClick={() => navigate(-2)} style={{ width: "100px" }}>
+        navigate(-2)
+      </button>
       <Link to="/">home</Link>
-      <p>apps</p>
+      <div>apps</div>
       {appsRoute.map((appRoute) => {
         const path = appRoute.path!;
         return (
-          <Link key={path} to={`/${path}`}>
+          <Link key={path} to={`/${path}`} style={{ paddingLeft: "1rem" }}>
             {path}
           </Link>
         );
